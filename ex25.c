af5053807878750036f2b96552bef51d788f13d8
Helena <helena.petruz@gmail.com>
2026-06-08 23:10:17 -0300

    Refactor tetranacci function and main function
    Fix formatting and remove unnecessary whitespace.

---
+++ ex25.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int tetranacci(int);
+
+int main() {
+    int n;
+    printf("Digite um valor para n: ");
+    scanf("%d", &n);
+    printf("Resultado = %d", tetranacci(n));
+}
+
+int tetranacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 0;
+    if (n == 3)
+        return 1;
+    return tetranacci(n - 1) +
+           tetranacci(n - 2) +
+           tetranacci(n - 3) +
+           tetranacci(n - 4);
+}
